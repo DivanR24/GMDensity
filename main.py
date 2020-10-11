feedffaa1996642f6f692def3d04cab867ba8a44
DivanR24 <DivanR24@students.spprep.org>
2020-10-11 01:05:58 +0000

    "GMDensityFlask.html" -> "index.html"

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ gmap = gmplot.GoogleMapPlotter(40.721643, -74.070558, 30, apikey='AIzaSyD7k0YLCt
 gmap.heatmap(coordinates.lat, coordinates.lon)#
 
 gmap.scatter(coordinates.lat, coordinates.lon, c='r', marker=False, alpha = 0.1, size = 14)
-gmap.draw('GMDensityFlask.html')
+gmap.draw('index.html')
 
