78b721c3f0014344d4027b662c278225c92241ae
DivanR24 <DivanR24@students.spprep.org>
2020-10-11 01:11:36 +0000

    zoom to 16 (was 30)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import gmplot
 import pandas as pd
 coordinates = pd.read_csv('coordination.csv')
-gmap = gmplot.GoogleMapPlotter(40.721643, -74.070558, 30, apikey='AIzaSyD7k0YLCtod0NTuxmBMej0Cf70LdCzZSx4')
+gmap = gmplot.GoogleMapPlotter(40.721643, -74.070558, 16, apikey='AIzaSyD7k0YLCtod0NTuxmBMej0Cf70LdCzZSx4', )
 gmap.heatmap(coordinates.lat, coordinates.lon)#
 
 gmap.scatter(coordinates.lat, coordinates.lon, c='r', marker=False, alpha = 0.1, size = 14)
